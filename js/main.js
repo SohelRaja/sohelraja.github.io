a0e87996d53f67fef1fcec4fa7b7aa6d0f970f2f
SohelRaja <rohan.nsec@gmail.com>
2019-10-24 20:08:37 +0530

    Skills Section Updated

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
 		closeBtnInside: true,
 		preloader: false,
 		midClick: true,
-		removalDelay: 300,
-		mainClass:'my-mfp-zoom-in'
+		removalDelay: 400,
+		mainClass:'mfp-fade'
 	});
 });
